5021eb2789cd6f93f6877ddd46332165d74d21e1
valentinsvt <valentinsvt@hotmail.com>
2015-06-22 10:38:22 -0500

    sin spiner y agregados los iconos.... (face palm)

--- app/src/main/java/nth/com/ares/MainActivity.java
+++ app/src/main/java/nth/com/ares/MainActivity.java
@@ -218,7 +218,7 @@ public class MainActivity extends AppCompatActivity implements NavigationDrawerC
                     android.os.Message msg = android.os.Message.obtain(null, ChatService2.MSG_SET_INT_VALUE, intvaluetosend, 0);
                     msg.replyTo = mMessenger;
                     mService.send(msg);
-                    showProgress(true);
+
                 } catch (RemoteException e) {
                 }
             }
@@ -396,41 +396,7 @@ public class MainActivity extends AppCompatActivity implements NavigationDrawerC
     }
 
 
-    /**
-     * Shows the progress UI and hides the login form.
-     */
-    @TargetApi(Build.VERSION_CODES.HONEYCOMB_MR2)
-    public void showProgress(final boolean show) {
-        // On Honeycomb MR2 we have the ViewPropertyAnimator APIs, which allow
-        // for very easy animations. If available, use these APIs to fade-in
-        // the progress spinner.
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB_MR2) {
-            int shortAnimTime = getResources().getInteger(android.R.integer.config_shortAnimTime);
 
-            mLayoutMain.setVisibility(show ? View.GONE : View.VISIBLE);
-            mLayoutMain.animate().setDuration(shortAnimTime).alpha(
-                    show ? 0 : 1).setListener(new AnimatorListenerAdapter() {
-                @Override
-                public void onAnimationEnd(Animator animation) {
-                    mLayoutMain.setVisibility(show ? View.GONE : View.VISIBLE);
-                }
-            });
-
-            mProgressView.setVisibility(show ? View.VISIBLE : View.GONE);
-            mProgressView.animate().setDuration(shortAnimTime).alpha(
-                    show ? 1 : 0).setListener(new AnimatorListenerAdapter() {
-                @Override
-                public void onAnimationEnd(Animator animation) {
-                    mProgressView.setVisibility(show ? View.VISIBLE : View.GONE);
-                }
-            });
-        } else {
-            // The ViewPropertyAnimator APIs are not available, so simply show
-            // and hide the relevant UI components.
-            mProgressView.setVisibility(show ? View.VISIBLE : View.GONE);
-            mLayoutMain.setVisibility(show ? View.GONE : View.VISIBLE);
-        }
-    }
 
 
     public void loadMensajes() {
