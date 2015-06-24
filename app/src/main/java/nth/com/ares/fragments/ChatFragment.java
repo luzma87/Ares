ee2e9830f47a8d65399aee7719ebb1009314f33f
valentinsvt <valentinsvt@hotmail.com>
2015-06-24 11:13:02 -0500

    padding en el chatfragment... arreglo de fechas en mensajes cargados de la base de datos.... no cambie los colores no mas xq soy chevere

--- app/src/main/java/nth/com/ares/fragments/ChatFragment.java
+++ app/src/main/java/nth/com/ares/fragments/ChatFragment.java
@@ -273,7 +273,7 @@ public class ChatFragment extends Fragment {
     public void showMessage(Mensaje mensaje) {
         String sender = mensaje.sender;
         String mns = mensaje.body;
-        String fecha = mensaje.fecha;
+        String fecha = mensaje.fecha.substring(10,19);
         Boolean mio = mensaje.esMio();
         showMessage(mio, sender, mns, fecha);
     }
